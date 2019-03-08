a599eeef8499a900c88ca8e907cecbc6c2898c41
Sam Joseph <tansaku@gmail.com>
2019-03-08 14:15:30 +0000

    notify users about browsers being older and out of date

--- js/app.js
+++ js/app.js
@@ -43,3 +43,13 @@ function scrolledPast(element) {
 document.addEventListener("scroll", () => {
   scrolledPast(logo);
 });
+
+// from https://browser-update.org/#install
+var $buoop = {required:{e:-4,f:-3,o:-3,s:-1,c:-3},insecure:true,api:2019.03 }; 
+function $buo_f(){ 
+ var e = document.createElement("script"); 
+ e.src = "//browser-update.org/update.min.js"; 
+ document.body.appendChild(e);
+};
+try {document.addEventListener("DOMContentLoaded", $buo_f,false)}
+catch(e){window.attachEvent("onload", $buo_f)}
